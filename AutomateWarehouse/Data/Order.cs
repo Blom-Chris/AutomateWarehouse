f58b09bd8fcd41ce92d8d6dd5e7c645c01a3c159
Blom-Chris <cribbe89@hotmail.com>
2021-10-18 12:29:22 +0200

    Orders updated

---
+++ AutomateWarehouse/Data/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomateWarehouse.Data
+{
+  public class Order
+  {
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string DeliveryAddress { get; set; }
+    public bool PaymentCompleted { get; set; }
+    public bool Dispatched { get; set; }
+    public List<OrderLine> Items { get; set; }
+  }
+}
