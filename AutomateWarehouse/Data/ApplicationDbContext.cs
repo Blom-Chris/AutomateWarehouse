2c3d5f5017b7ce397d81b0f8f6ca1a3d1c02d101
Blom-Chris <cribbe89@hotmail.com>
2021-10-18 13:10:07 +0200

    DbContext

---
+++ AutomateWarehouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomateWarehouse.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+      Database.EnsureCreated();
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderLine> OrderLines { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Author>().HasMany(d => d.Books).WithOne(e => e.Author);
+
+      modelBuilder.Entity<Author>().HasMany(d => d.Books).WithOne(e => e.Author);
+
+      base.OnModelCreating(modelBuilder);
+    }
+  }
+}
