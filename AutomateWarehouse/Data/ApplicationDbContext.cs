d23c0bf1214e6ca00cb2515a863fdb82171dd1be
Blom-Chris <cribbe89@hotmail.com>
2021-10-18 14:17:02 +0200

    DbContext

--- AutomateWarehouse/Data/ApplicationDbContext.cs
+++ AutomateWarehouse/Data/ApplicationDbContext.cs
@@ -8,23 +8,61 @@ namespace AutomateWarehouse.Data
 {
   public class ApplicationDbContext : DbContext
   {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-      Database.EnsureCreated();
-    }
-
     public DbSet<Product> Products { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderLine> OrderLines { get; set; }
 
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+      Database.EnsureCreated();
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      modelBuilder.Entity<Author>().HasMany(d => d.Books).WithOne(e => e.Author);
+      modelBuilder.Entity<OrderLine>().HasOne(o => o.Product);
+      modelBuilder.Entity<OrderLine>().HasOne(o => o.Order);
+      //modelBuilder.Entity<Order>().HasMany(o => o.Items);
+      modelBuilder.Entity<Order>().HasOne(o => o.Customer);
+      modelBuilder.Entity<Customer>().HasMany(o => o.Orders);
 
-      modelBuilder.Entity<Author>().HasMany(d => d.Books).WithOne(e => e.Author);
+      modelBuilder.Entity<Customer>().HasData(GetCustomers());
+      modelBuilder.Entity<Product>().HasData(GetProducts());
+      modelBuilder.Entity<Order>().HasData(GetOrders());
+      modelBuilder.Entity<OrderLine>().HasData(GetOrderLines());
 
       base.OnModelCreating(modelBuilder);
+    }
+
+    private List<Customer> GetCustomers()
+    {
+      return new List<Customer>
+      {
+      new Customer { Id =901204 , Name ="Ano Nym", Phone ="0705555555", Email ="ano@gmail.com"},
+      new Customer { Id =880417 , Name ="Test Testsson", Phone ="0704444444", Email ="t.test@outlook.com"}
+      };
+    }
+    private List<Product> GetProducts()
+    {
+      return new List<Product>
+      { 
+        new Product { Id = 1, Name = "Cake", Price = 12.46 , Stock = 10, Description ="The cake is a lie" },
+        new Product { Id = 2, Name = "A blue pen", Price = 5.99 , Stock = 50, Description ="It ’s a bluepen" },
+    };
+    }
+    private List<Order> GetOrders()
+    {
+      return new List<Order>
+      { 
+        new Order { Id = 991, CustomerId = GetCustomers()[0].Id , DeliveryAddress= "Hamngatan 3", Dispatched = false , OrderDate = DateTime .Now .Date , PaymentCompleted = false }
+    };
+    }
+    private List<OrderLine> GetOrderLines()
+    {
+      return new List<OrderLine>
+      { 
+        new OrderLine { Id = 1234 , Quantity = 3, ProductId = GetProducts()[0].Id , OrderId = GetOrders () [0]. Id }
+      };
     }
   }
 }
--- AutomateWarehouse/Startup.cs
+++ AutomateWarehouse/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,8 +29,14 @@ namespace AutomateWarehouse
     {
       services.AddRazorPages();
       services.AddServerSideBlazor();
-      services.AddSingleton<WeatherForecastService>();
+
+      services.AddDbContext<ApplicationDbContext>(options => 
+      {
+        options.UseSqlite("Data Source = AutomateWarehouse.db");
+      });
     }
+
+
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
