ef97197661daf192d37b8667bf7f2ad98dcd2286
PetterssonJohannes <johannes.pettersson.8305@student.uu.se>
2021-10-20 09:43:36 +0200

    Orders

--- AutomateWarehouse/Data/IOrderRepository.cs
+++ AutomateWarehouse/Data/IOrderRepository.cs
@@ -11,6 +11,9 @@ namespace AutomateWarehouse.Data
 
         Task<Order> AddNewOrderAsync(Order order);
         Task<Order> PayOrderAsync(Order order);
+        Task<Order> EditOrderAsync(Order order);
+        Task<Order> UndoPaymentAsync(Order order);
+        Task<Order> UndoDispatchAsync(Order order);
         Task ProcessOrderAsync();
         Task<List<Order>> GetAllDispatchedOrdersAsync();
         Task<List<Order>> GetAllPendingOrdersAsync();
